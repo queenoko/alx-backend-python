afd916f51e84146bdcd093c7a411309f6fb84a8b
queenoko <quimaudongwo@gmail.com>
2024-01-11 15:44:38 +0100

    sum anotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This Type-annotated function sum_list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a list of floats and returns their sum as float"""
+    y: float = 0.0
+    for z in input_list:
+        y += z
+        return y
